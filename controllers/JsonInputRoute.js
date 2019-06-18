27ab5fe24d7b3def32b5e7321954cf29859c5ffd
Subham Biswas <subham.biswas@alaskaair.com>
2019-06-18 16:13:04 +0530

    third commit

--- controllers/JsonInputRoute.js
+++ controllers/JsonInputRoute.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const fs = require('fs');
+var os = require("os");
 
 function JsonRequest(req, res, next) {
     console.log(req.body);      // your JSON
     res.send(req.body);
-    fs.appendFile("temp.txt", req.body.MachineId, (err) => {
+    fs.appendFile("temp.txt",JSON.stringify(req.body), (err) => {
         if (err) console.log(err);
-        console.log("Json Machine Id Inputted to file");
-    })
-    for (var contentValue of req.body.content) {
-        fs.appendFile("temp.txt", contentValue, (err) => {
-            if (err) console.log(err);
-            console.log("Json Content Inputted to file");
-        });
-    }// echo the result back
-};
+        console.log("Json Inputted to file");
+    });
+    // for (var contentValue in req.body.content) {
+    //     fs.appendFile("temp.txt", contentValue, (err) => {
+    //         if (err) console.log(err);
+    //         console.log("Json Content Inputted to file");
+    //     });
+    // }// echo the result back
+    fs.appendFile("temp.txt",os.EOL, (err) => {
+        if (err) console.log(err);
+        console.log("Json new line Inputted to file");
+    });
+}
 
 module.exports = JsonRequest;
